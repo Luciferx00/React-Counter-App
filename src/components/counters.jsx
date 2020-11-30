02f711b9ad270bee731bf07ddb6302d7ff48a905
Luciferx00 <apurvakumarg@gmail.com>
2020-11-30 20:26:42 +0530

    first commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    const {
+      onReset,
+      onIncrement,
+      onDelete,
+      onDecrement,
+      counters
+    } = this.props;
+    return (
+      <div>
+        <button
+          className="btn btn-success m-2"
+          onClick={onReset}
+          disabled={counters.length === 0 ? "disabled" : ""}
+        >Reset
+          <i className="fa fa-refresh" aria-hidden="true" />
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
